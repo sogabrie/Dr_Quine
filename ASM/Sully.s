45b8793bb01df183e541416b9d30e83bfcfd24cc
sogabrie <gabrielyan.sos@mail.ru>
2025-06-13 04:11:09 +0400

    update

--- ASM/Sully.s
+++ ASM/Sully.s
@@ -2,7 +2,7 @@ BITS 64
 
 section .data
     i: dq 5
-    newfile: db "", 0
+    newfile: db "BITS 64%3$c%3$csection .data%3$c    i: dq %5$d%3$c    newfile: db %4$c%1$s%4$c, 0%3$c    flag: db %4$cw%4$c, 0%3$c    filename: db %4$cSully_%%d.s%4$c, 0%3$c    exe: db %4$cSully_%%d%4$c, 0%3$c    compilation: db %4$cnasm -f elf64  %%s.s -o %%s.o && gcc -z execstack -no-pie %%s.o -o %%s%4$c, 0%3$c    runexe: db %4$c./%%s%4$c, 0%3$c%3$csection .bss%3$c    exeN resb 100%3$c    fileN resb 100%3$c    runE resb 200%3$c    compil resb 300%3$c%3$csection .text%3$c    extern fprintf, fopen, fclose, sprintf, system%3$c    global main%3$c%3$cmain:%3$c    mov rax, [i]%3$c    cmp rax, 0%3$c    jle .end%3$c%3$c    cmp rax, 5%3$c    jz .continu%3$c    dec rax%3$c    mov r13, rax%3$c%3$c.continu:%3$c    mov rdi, fileN%3$c    mov rsi, filename%3$c    mov rdx, rax%3$c    call sprintf%3$c%3$c    mov rdi, exeN%3$c    mov rsi, exe%3$c    mov rdx, rax%3$c    call sprintf%3$c%3$c    mov rdi, runE%3$c    mov rsi, runexe%3$c    mov rdx, exeN%3$c    call sprintf%3$c%3$c    mov rdi, compil%3$c    mov rsi, compilation%3$c    mov rdx, exeN%3$c    call sprintf%3$c%3$c    mov rdi, fileN%3$c    mov rsi, flag%3$c    call fopen%3$c    test rax, rax%3$c    jz .end%3$c    mov r12, rax%3$c%3$c    mov r10, r13%3$c    sub rsp, 8%3$c    mov [rsp], r10%3$c%3$c    mov rdi, r12%3$c    lea rsi, [rel newfile]%3$c    lea rdx, [rel newfile]%3$c    mov rcx, 9%3$c    mov r8, 10%3$c    mov r9, 34%3$c    xor rax, rax%3$c    call fprintf%3$c    add rsp, 8%3$c    mov rdi, r12%3$c    call fclose%3$c%3$c    mov rdi, compil%3$c    call system%3$c%3$c    mov rdi, runE%3$c    call system%3$c%3$c.end:%3$c    mov rax, 60%3$c%2$cxor rdi, rdi%3$c%2$csyscall", 0
     flag: db "w", 0
     filename: db "Sully_%d.s", 0
     exe: db "Sully_%d", 0
@@ -58,8 +58,6 @@ main:
     mov r12, rax
 
     mov r10, r13
-    sub rsp, 8
-    mov [rsp], r10
 
     mov rdi, r12
     lea rsi, [rel newfile]
@@ -67,9 +65,11 @@ main:
     mov rcx, 9
     mov r8, 10
     mov r9, 34
+    sub rsp, 16
+    mov qword [rsp], r10
     xor rax, rax
     call fprintf
-    add rsp, 8
+    add rsp, 16
     mov rdi, r12
     call fclose
 
